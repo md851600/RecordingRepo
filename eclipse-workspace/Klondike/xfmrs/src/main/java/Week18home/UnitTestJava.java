c2379dfe2b77b83c4c923c5d5f74c66f576f2df8
md851600 <123835346+md851600@users.noreply.github.com>
2023-02-21 20:18:03 -0500

    Update UnitTestJava.java
    Feb 21 lecture about updates

--- eclipse-workspace/Klondike/xfmrs/src/main/java/Week18home/UnitTestJava.java
+++ eclipse-workspace/Klondike/xfmrs/src/main/java/Week18home/UnitTestJava.java
@@ -2,10 +2,11 @@ package Week18home;
 
 public class UnitTestJava {
 	//Feb 15 BrynMawr->MDJunirs cannot get online
-	
+	// Feb 21 add variable c
 	public static void main(String[] args) {
 	int a = 5;
 	int b = 7;
-	System.out.println(a+b);
+		int c = 9;
+	System.out.println(a+b-c);
 	}
 }
