df76869f1edd1930a429e2ac0cf85f34b0e566bc
Juan Zhong <123835346+md851600@users.noreply.github.com>
2025-03-06 17:25:44 -0500

    Update Testim.js

--- Testim.js
+++ Testim.js
@@ -1,3 +1,12 @@
 //march 5 2025 bus DC to Brokenland p&r
 import 
-  main (
+  public static void main(String[] args) throws IOException {
+		// march 6 335 cooy from Magician
+
+		WebDriverManager.edgedriver().setup();
+		WebDriver driver = new EdgeDriver();
+		//driver.manage().window().maximize(); Jan 25 add screen shots
+		 // Jan23, this runs from main directory, comment out in porm.xml
+		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		 driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+		driver.get("https://www.hotmail.com");
